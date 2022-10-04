15b1a1ed111a53e21c05f1bc75e39e487ce7f6df
Souradeep Banerjee <rishibanerjee1101@gmail.com>
2022-10-04 12:08:07 +0530

    Initial Commit: Added a splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:yoga_app/services/auth_gate.dart';
+import 'package:yoga_app/screens/splash.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         //primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const AuthGate(),
+      home: const Splash(),
     );
   }
 }
---
+++ lib/screens/splash.dart
@@ -0,0 +1,76 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../services/auth_gate.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3),
+          ()=>Navigator.pushReplacement(context,
+        MaterialPageRoute(builder:
+            (context) =>
+            const AuthGate(),
+        ),
+      ),
+    );
+  }
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.white,
+                Colors.cyan[100] as Color,
+              ],
+            ),
+            border: Border.all(
+              color: Colors.cyan[100] as Color,
+              width: 1,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.cyan[100] as Color,
+                offset: const Offset(0,5),
+                blurRadius: 6,
+              ),
+              const BoxShadow(
+                color: Colors.white,
+                offset: Offset(0.0, 0.0),
+                blurRadius: 0.0,
+                spreadRadius: 0.0,
+              ),
+            ]
+        ),
+        child: Center(
+          child: Container(
+            width: 288,
+            height: 288,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/splash_logo.png'),
+                  fit: BoxFit.cover,
+                )
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
